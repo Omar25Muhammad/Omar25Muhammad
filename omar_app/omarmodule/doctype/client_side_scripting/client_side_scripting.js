ded8f6c0b54f8124f4609f54949e2817d5a61310
omar25Muhammad <maitreomar25@yahoo.com>
2022-07-14 20:50:42 +0200

    Hi, There

---
+++ omar_app/omarmodule/doctype/client_side_scripting/client_side_scripting.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2022, Me and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Client Side Scripting', {
+  // Those are called events
+  /*
+        msgprint: as info
+        throw: as error
+
+  */
+	// refresh: function(frm) {
+  //   frappe.msgprint('Top of morning to you!')
+	// }
+
+  // onload: function(frm) {
+  //   frappe.msgprint('Onloada event')
+  // }
+
+  // validate: function(frm) {
+  //   frappe.msgprint('Validate event') // when saving the document
+  // }
+
+  // before_save: function(frm) {
+  //   frappe.throw('Before Saving event') // it will not make me save the doc
+  // }
+
+  // after_save: function(frm) {
+  //   frappe.throw('After Saving event')
+  // }
+
+  // enable: function(frm) {
+  //   frappe.msgprint('You\'ve modified the enable check')
+  // }
+
+  // age: function(frm) {
+  //   frappe.msgprint('You\'ve entered your age')
+  // }
+
+  // before_submit: function(frm) {
+  //   frappe.msgprint('You are AWARE that once submit the doc, there is NO way back. Right?')
+  // }
+
+  // ####################################################
+
+  // Fetching Data //
+
+  // before_save:function(frm){
+  //   frappe.msgprint(__('The full name is "{0}"', [frm.doc.first_name+" "+frm.doc.middle_name]))
+  // }
+
+});
